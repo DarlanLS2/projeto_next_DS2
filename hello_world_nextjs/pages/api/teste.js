5bfc49d324c8d93ca0cad327a869fbe658df51e3
DarlanLS2 <darlanleite.arnaldo@gmail.com>
2025-03-26 20:17:30 -0300

    adicionei um teste.js e passo-a-passo, o passo-a-passo é apenas um arquivo txt com o que fazer para rodar e o teste.js é uma pagina que tem uma conexao com o banco de dados e retorna um select all de uma tabela

---
+++ hello_world_nextjs/pages/api/teste.js
@@ -0,0 +1,24 @@
+import mysql from 'mysql2';
+
+// conexao com o banco 
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'bancoteste'
+});
+
+// O que sera retornado na rota /api/teste
+export default function handle(req, res) {
+    /* Query que retorna um erro e resultado 
+    e roda uma função para lidar com eles*/
+    connection.query('SELECT * FROM tabela', (error, results) => {
+        // Lidando com o erro
+        if (error) {
+            console.error('Erro na consulta', error);
+            return res.json.status(500).json({error: 'Erro ao buscar dados'});
+        }
+        // Lidando com o resultado
+        res.status(200).json(results);
+    })
+}
